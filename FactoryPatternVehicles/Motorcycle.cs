fe5c2b9147be88db8b242632e79ff629b750ffbb
Jared Lewis <jlewis8493@gmail.com>
2022-01-28 11:37:30 -0500

    Factory Pattern Exercise 1 Complete

---
+++ FactoryPatternVehicles/Motorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FactoryPatternVehicles
+{
+    public class Motorcycle : IVehicle
+    {
+        public Motorcycle()
+        {
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("The Motorcycle is revving up");
+            Console.WriteLine("vrooooooom");
+        }
+    }
+}
